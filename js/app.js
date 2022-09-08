c310271f321ea6574663bd69f9803ea0e65624d3
faysal hossain <faysal15-14743@diu.edu.bdy 			+extext??!.EXIT>
2022-09-08 21:44:15 +0600

    modal update

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 const catagorys = () => {
     const url = `https://openapi.programming-hero.com/api/news/categories`;
     fetch(url)
-    .then(res => res.json())
-    .then(data => getCategoryNews(data.data.news_category))
+        .then(res => res.json())
+        .then(data => getCategoryNews(data.data.news_category))
 }
 const getCategoryNews = (categorys) => {
     // console.log(categorys)
@@ -18,22 +18,22 @@ const getCategoryNews = (categorys) => {
         `
         divcatagory.appendChild(newUl)
     });
-    
+
 }
 
 const showCategoryNews = async category_id => {
-    const url =`https://openapi.programming-hero.com/api/news/category/${category_id}`;
-    const res = await  fetch(url)
+    const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`;
+    const res = await fetch(url)
     const data = await res.json();
     displayCategoryNews(data.data)
-    
+
 }
 
 const displayCategoryNews = (newss) => {
     const itemFound = document.getElementById('item-number');
-    itemFound.innerText  = newss.length;
+    itemFound.innerText = newss.length;
     const foundCatagoryName = document.getElementById('catagory-name');
-    
+
 
 
     const newsCaed = document.getElementById('display-news');
@@ -43,40 +43,57 @@ const displayCategoryNews = (newss) => {
         const cardDiv = document.createElement('div');
         cardDiv.classList.add('col')
         cardDiv.innerHTML = `
-        <div class="card mb-3" >
-              <div class="row g-0">
-                <div class="col-md-4">
-                  <img src="${news.thumbnail_url}" class="img-fluid rounded-start" alt="..." />
-                </div>
-                <div class="col-md-8">
-                  <div class="card-body">
-                    <h5 class="card-title">${news.title}</h5>
-                    <p class="card-text">${news.details.slice(0 ,300)}...</p>
-                    
-                    <div class="d-flex align-items-center">
-                <div>
-                  <img
-                    src="${news.author.img}"
-                    class="rounded-circle faysal"
-                    height="30"
-                    alt="Portrait of a Woman"
-                    loading="lazy"
-                  />
-                </div>
-                <div class="fs-6 px-3">
-                  <h6 class="">${news.author.name ? news.author.name : 'no name' }</h6>
-                  <small class="text-muted">${news.author.published_date ? news.author.published_date : 'No dete' }</small>
-                </div>
-                <div class="mx-4">
-                  <small>view ${news.total_view} </small>
-                </div>
-                <a class="btn" href="#" >  </a>
-              </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-        `
+    <div class="card mb-3">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img
+        src="${news.thumbnail_url}"
+        class="img-fluid rounded-start"
+        alt="..."
+      />
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">${news.title}</h5>
+        <p class="card-text">${news.details.slice(0, 300)}...</p>
+        <div class="d-flex align-items-center">
+          <div class="ms-5">
+            <img
+              src="${news.author.img}"
+              class="rounded-circle faysal"
+              height="40"
+              alt="Portrait of a Woman"
+              loading="lazy"
+            />
+          </div>
+
+          <div class="fs-6 px-3">
+            <h6 class="">${news.author.name ? news.author.name : 'no name'}</h6>
+            <small class="text-muted"
+              >${news.author.published_date ? news.author.published_date : 'No dete'}</small
+            >
+          </div >
+
+          <div class="mx-5">
+            <small><i class="fa-solid fa-eye"></i> ${news.total_view} </small>
+          </div>
+
+          <div class="mx-5">
+            <button
+              class="btn "
+              data-bs-toggle="modal"
+              data-bs-target="#exampleModal"
+              onclick=""
+            >
+              <i class="fa-solid fa-circle-chevron-right"></i>
+            </button>
+          </div>
+        </div >
+      </div >
+    </div >
+  </div >
+</div >
+`
         newsCaed.appendChild(cardDiv)
         // creat display
 
